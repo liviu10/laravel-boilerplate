291b330816d304bf748ab85f007db25168d103e0
Liviu Voica <voica.liviu10@gmail.com>
2023-03-12 08:25:59 +0200

    Configured the guest layout

---
+++ backend/resources/views/layouts/guest.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- CSRF TOKEN -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>{{ config('app.name') }} | {{ config('app.owner_name') }}</title>
+        <!-- BOOTSTRAP V5 CSS IMPORT LINK -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <!-- FONT AWESOME V6.3.0 IMPORT LINK -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- GOOGLE FONTS IMPORT LINK -->
+        <link rel="dns-prefetch" href="//fonts.gstatic.com">
+        <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+        <!-- LOCAL CSS AND JAVASCRIPT -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    </head>
+    <body>
+        <div id="app">
+            <main class="container-fluid">
+                @yield('content')
+            </main>
+        </div>
+
+        <!-- BOOTSTRAP V5 BUNDLE WITH POPPER.JS IMPORT LINK -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    </body>
+</html>
