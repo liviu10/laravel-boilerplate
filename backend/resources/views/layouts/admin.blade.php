74c2deef30b93f5ff7f981eb6ef787b9c613518a
Liviu Voica <voica.liviu10@gmail.com>
2023-03-13 21:40:48 +0200

    Configured separate layouts for admin and guest

--- backend/resources/views/layouts/admin.blade.php
+++ backend/resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- CSRF TOKEN -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>{{ config('app.name') }} | {{ config('app.owner_name') }}</title>
+        <title>Admin | {{ config('app.name') }} | {{ config('app.owner_name') }}</title>
         <!-- BOOTSTRAP V5 CSS IMPORT LINK -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- FONT AWESOME V6.3.0 IMPORT LINK -->
@@ -14,7 +14,7 @@
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
         <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
         <!-- LOCAL CSS AND JAVASCRIPT -->
-        @vite(['resources/sass/guest.scss', 'resources/js/app.js'])
+        @vite(['resources/sass/admin.scss', 'resources/js/app.js'])
     </head>
     <body>
 
