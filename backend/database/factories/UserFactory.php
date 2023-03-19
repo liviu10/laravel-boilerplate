6a803228d8f0f18e93f6bbeb15d86aaba9e624d0
Liviu Voica <voica.liviu10@gmail.com>
2023-03-19 22:01:06 +0200

    Bug fixing the UserFactory

--- backend/database/factories/UserFactory.php
+++ backend/database/factories/UserFactory.php
@@ -21,12 +21,13 @@ class UserFactory extends Factory
             'full_name'         => fake()->name(),
             'first_name'        => fake()->firstName(),
             'last_name'         => fake()->lastName(),
-            'nickname'          => explode('@', fake()->unique()->safeEmail()),
+            'nickname'          => fake()->userName(),
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'user_role_type_id' => rand(1, 5),
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
+            'profile_image'     => null,
         ];
     }
 
