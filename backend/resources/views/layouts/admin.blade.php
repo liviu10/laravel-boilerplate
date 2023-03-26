e5e44d4b64b46824343e760c1dd60df17f3b30c1
Liviu Voica <voica.liviu10@gmail.com>
2023-03-26 12:43:37 +0300

    Bug fixing when displaying the app owner in the admin layout

--- backend/resources/views/layouts/admin.blade.php
+++ backend/resources/views/layouts/admin.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <!-- CSRF TOKEN -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        <title>Admin | {{ config('app.name') }} | {{ config('app.owner_name') }}</title>
+        <title>Admin | {{ config('app.name') }} | {{ config('app.owner') }}</title>
         <!-- FONT AWESOME V6.3.0 IMPORT LINK -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         <!-- GOOGLE FONTS IMPORT LINK -->
