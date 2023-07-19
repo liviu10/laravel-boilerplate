549c3c136369f48f8692a29fbb4b62cd206c6f41
Liviu Voica <voica.liviu10@gmail.com>
2023-07-19 08:14:03 +0300

    Added pinia store for admin resources

---
+++ frontend/src/stores/admin/management/articles.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+
+const useArticleStore = defineStore('articleStore', {
+  state: () => ({
+    allRecords: {},
+    createRecord: {},
+    findRecord: {},
+    updateRecord: {},
+    deleteRecord: {},
+  }),
+  getters: {
+    getAllRecords: (state) => state.allRecords,
+    getCreatRecord: (state) => state.createRecord,
+    getFindRecord: (state) => state.findRecord,
+    getUpdateRecord: (state) => state.updateRecord,
+    getDeleteRecord: (state) => state.deleteRecord,
+  },
+  actions: {
+    async getRecords() {
+      debugger;
+    },
+
+    async createRecord() {
+      debugger;
+    },
+
+    async findRecord() {
+      debugger;
+    },
+
+    async updateRecord() {
+      debugger;
+    },
+
+    async deleteRecord() {
+      debugger;
+    }
+  },
+});
+
+export { useArticleStore };
---
+++ frontend/src/stores/admin/userSettings/userProfile.ts
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia';
+
+const useUserSettingStore = defineStore('userSettingStore', {
+  state: () => ({
+    allRecords: {},
+    createRecord: {},
+    findRecord: {},
+    updateRecord: {},
+    deleteRecord: {},
+  }),
+  getters: {
+    getAllRecords: (state) => state.allRecords,
+    getCreatRecord: (state) => state.createRecord,
+    getFindRecord: (state) => state.findRecord,
+    getUpdateRecord: (state) => state.updateRecord,
+    getDeleteRecord: (state) => state.deleteRecord,
+  },
+  actions: {
+    async getRecords() {
+      debugger;
+    },
+
+    async createRecord() {
+      debugger;
+    },
+
+    async findRecord() {
+      debugger;
+    },
+
+    async updateRecord() {
+      debugger;
+    },
+
+    async deleteRecord() {
+      debugger;
+    }
+  },
+});
+
+export { useUserSettingStore };
