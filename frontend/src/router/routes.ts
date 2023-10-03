ce1711d2ce856e2d01356976304cb7301965639b
Liviu Voica <voica.liviu10@gmail.com>
2023-10-03 08:12:31 +0300

    Remove api endpoint and columns because these already exist in backend

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,8 +1,4 @@
 import { RouteRecordRaw } from 'vue-router';
-import management from 'src/router/management';
-import communication from 'src/router/communication';
-import userSettings from 'src/router/userSettings';
-import applicationSettings from 'src/router/applicationSettings';
 
 const routes: RouteRecordRaw[] = [
   {
@@ -19,40 +15,6 @@ const routes: RouteRecordRaw[] = [
           icon: 'home',
         },
       },
-
-      // Admin management urls,
-      ...management,
-
-      // Admin communication urls,
-      ...communication,
-
-      // {
-      //   path: '/admin/reports',
-      //   name: 'MonitoringPage',
-      //   component: () => import('pages/admin/MonitoringPage.vue'),
-      //   meta: {
-      //     title: 'admin.reports.title',
-      //     caption: 'admin.reports.description',
-      //     icon: 'analytics',
-      //   },
-      // },
-
-      {
-        path: '/admin/documentation',
-        name: 'DocumentationPage',
-        component: () => import('pages/admin/DocumentationPage.vue'),
-        meta: {
-          title: 'admin.documentation.title',
-          caption: 'admin.documentation.description',
-          icon: 'help',
-        },
-      },
-
-      // Admin user settings urls
-      ...userSettings,
-
-      // Admin application settings urls
-      ...applicationSettings,
     ],
   },
 
