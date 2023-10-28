bd29fccdf1ef92e2aa2ea9acef4ffe8346fed7a3
Liviu Voica <voica.liviu10@gmail.com>
2023-10-28 17:30:40 +0300

    Work in progress components

--- backend/resources/views/components/breadcrumb.blade.php
+++ backend/resources/views/components/breadcrumb.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Walk as if you are kissing the Earth with your feet. - Thich Nhat Hanh -->
+<div class="section section--breadcrumb">
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item">
+                <a href="#">
+                    Home
+                </a>
+            </li>
+            <li class="breadcrumb-item">
+                <a href="#">
+                    Library
+                </a>
+            </li>
+            <li class="breadcrumb-item active" aria-current="page">
+                Data
+            </li>
+        </ol>
+    </nav>
 </div>
--- backend/resources/views/components/card.blade.php
+++ backend/resources/views/components/card.blade.php
@@ -1,3 +1,19 @@
-<div>
-    <!-- Because you are alive, everything is possible. - Thich Nhat Hanh -->
+<div class="section section--card">
+    <div class="card">
+        <img
+            src="https://images.unsplash.com/photo-1594069758873-e79e9075eb7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+            class="card-img-top"
+            alt="..."
+            width="320"
+            height="180"
+        >
+        <div class="card-body">
+            <p class="card-title">Card title</p>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+        <div class="card-actions">
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+    </div>
 </div>
