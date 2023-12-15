c1f266b6caadf5e8e46bb3e7f4e616729fba8eca
liviu <liviu@Ubuntu-OS.myguest.virtualbox.org>
2023-12-15 14:28:29 +0200

    Configured a basic table component

---
+++ frontend/src/components/BasicTable.vue
@@ -0,0 +1,224 @@
+<template>
+  <q-table
+    :bordered="bordered"
+    class="admin-section__basic-table"
+    :columns="columns"
+    :dense="dense"
+    :grid="$q.screen.lt.sm"
+    :loading="loading"
+    :no-data-label="t('admin.generic.table_no_data_label')"
+    :rows="rows"
+    row-key="id"
+    :square="square"
+    :rows-per-page-options="rowsPerPageOptions"
+  >
+    <!-- Loading slot -->
+    <template v-slot:loading>
+      <q-inner-loading showing>
+        <q-spinner-gears size="60px" color="primary" />
+      </q-inner-loading>
+    </template>
+
+    <!-- Top left slot -->
+    <template v-slot:top-left>
+      <q-btn
+        color="primary"
+        dense
+        icon="add"
+        :label="t('admin.generic.create_record_label')"
+        square
+        @click="openDialog(actionMethods[0])"
+      >
+        <q-tooltip>
+          {{ t('admin.generic.create_record_tooltip') }}
+        </q-tooltip>
+      </q-btn>
+      <grid-table-top-left :action-methods="actionMethods" :more-options="moreOptions" />
+    </template>
+
+    <!-- Top right slot -->
+    <template v-slot:top-right>
+      <div class="admin-section__basic-table-top-right">
+        <q-form>
+          <q-input
+            dense
+            :label="t('admin.generic.search_the_resource')"
+            outlined
+            square
+            v-model="searchResource"
+          >
+            <template v-slot:append>
+              <q-icon name="search" class="cursor-pointer" />
+            </template>
+          </q-input>
+        </q-form>
+      </div>
+    </template>
+
+    <!-- Body slot -->
+    <template v-if="$q.screen.gt.xs" v-slot:body="props">
+      <q-tr :props="props">
+        <q-td v-for="col in props.cols" :key="col.name" :props="props">
+          <grid-table-body-actions
+            :action-methods="actionMethods"
+            :more-actions="moreActions(props.row)"
+          />
+        </q-td>
+      </q-tr>
+    </template>
+    <template v-if="$q.screen.lt.sm" v-slot:item="props">
+      <div class="admin-section__basic-table-content">
+        <q-card>
+          <q-card-section>
+            <grid-table-body :table-body-props="props" />
+            <grid-table-body-actions
+              :action-methods="actionMethods"
+              :more-actions="moreActions(props.row)"
+            />
+          </q-card-section>
+        </q-card>
+      </div>
+    </template>
+  </q-table>
+</template>
+
+<script setup lang="ts">
+// Import vue related utilities
+import { useI18n } from 'vue-i18n';
+import { Ref, computed, ref } from 'vue';
+import { QTableProps } from 'quasar';
+import { LocationQueryRaw, useRouter } from 'vue-router';
+
+// Import library utilities, interfaces and components
+import { TDialog, actionMethods } from 'src/interfaces/BaseInterface';
+import { HandleRoute } from 'src/utilities/HandleRoute';
+import GridTableTopLeft from './GridTableTopLeft.vue';
+import GridTableBody from './GridTableBody.vue';
+import GridTableBodyActions from './GridTableBodyActions.vue';
+
+// Defined the translation variable
+const { t } = useI18n({});
+
+interface IManagementGridTable {
+  bordered?: boolean;
+  columns?: QTableProps['columns'];
+  dense?: boolean;
+  grid?: boolean;
+  loading?: boolean;
+  rows?: QTableProps['rows'];
+  square?: boolean;
+  resource: string;
+  rowsPerPageOptions?: QTableProps['rowsPerPageOptions'];
+}
+
+const props = withDefaults(defineProps<IManagementGridTable>(), {
+  bordered: true,
+  customLoader: true,
+  dense: true,
+  grid: true,
+  square: true,
+  rowsPerPageOptions: () => [10, 25, 50, 100, 0],
+});
+
+// Navigate to route
+const navigateToRoute = new HandleRoute();
+
+// Go to Configure resource
+const router = useRouter();
+
+// More options
+const moreOptions = [
+  {
+    id: 1,
+    clickEvent: () => openDialog(actionMethods[4]),
+    icon: 'filter_alt',
+    label: 'admin.generic.advanced_filters_record_label',
+  },
+  {
+    id: 2,
+    clickEvent: () => openDialog(actionMethods[5]),
+    icon: 'upload',
+    label: 'admin.generic.upload_record_label',
+  },
+  {
+    id: 3,
+    clickEvent: () => openDialog(actionMethods[6]),
+    icon: 'download',
+    label: 'admin.generic.download_record_label',
+  },
+  {
+    id: 4,
+    clickEvent: () => openDialog(actionMethods[8]),
+    icon: 'restore_from_trash',
+    label: 'admin.generic.restore_record_label',
+  },
+  {
+    id: 5,
+    clickEvent: () =>
+      navigateToRoute.handleNavigateToRoute(
+        router,
+        'AdminSettingConfigurationResourcePage',
+        undefined,
+        ({ resource: props.resource } as unknown) as LocationQueryRaw
+      ),
+    icon: 'handyman',
+    label: 'admin.generic.configure_resource',
+  },
+];
+
+// Search resource
+let searchResource: Ref<string | number | null | undefined> = ref(null);
+
+// More actions
+const moreActions = computed(() => {
+  return (record: unknown) => {
+    return [
+      {
+        id: 1,
+        clickEvent: () => openDialog(actionMethods[1], record),
+        icon: 'visibility',
+        label: 'admin.generic.quick_show_record_label',
+      },
+      {
+        id: 2,
+        clickEvent: () => openDialog(actionMethods[2], record),
+        icon: 'edit',
+        label: 'admin.generic.quick_edit_record_label',
+      },
+      {
+        id: 3,
+        clickEvent: () => openDialog(actionMethods[3], record),
+        icon: 'delete',
+        label: 'admin.generic.delete_record_label',
+      },
+      {
+        id: 4,
+        clickEvent: () => openDialog(actionMethods[7], record),
+        icon: 'query_stats',
+        label: 'admin.generic.content_stats',
+      },
+    ];
+  };
+});
+
+// Action dialog
+const openDialog = (action: TDialog, record?: unknown): void => {
+  if (record && record !== undefined) {
+    if (record.hasOwnProperty('id')) {
+      return emit('handleOpenDialog', action, (record as { id: number }).id);
+    } else {
+      // TODO: notification to user that something went wrong
+    }
+  } else {
+    return emit('handleOpenDialog', action);
+  }
+};
+
+const emit = defineEmits<{
+  (event: 'handleOpenDialog', action: TDialog, recordId?: number): void;
+}>();
+</script>
+
+<style lang="scss" scoped>
+@import 'src/css/components/management_grid_table.scss';
+</style>
--- frontend/src/pages/AdminSettingConfigurationResourcePage.vue
+++ frontend/src/pages/AdminSettingConfigurationResourcePage.vue
@@ -1,31 +1,249 @@
 <template>
   <q-page class="admin admin--page">
-
     <page-title :page-title="t('admin.settings.configuration_resource.title')" />
 
-    <page-description :page-description="t('admin.settings.configuration_resource.page_description')" />
+    <page-description
+      :page-description="t('admin.settings.configuration_resource.page_description')"
+    />
+
+    <div class="admin-section admin-section--container">
+      <basic-table
+        :columns="configurationResourceStore.getColumns"
+        :resource="configurationResourceStore.resourceName"
+        :rows="configurationResourceStore.getAllRecords.results?.data || []"
+        @handle-open-dialog="handleOpenDialog"
+      />
+    </div>
 
     <page-loading :visible="loadPage" />
-
   </q-page>
 </template>
 
 <script setup lang="ts">
 // Import vue related utilities
 import { useI18n } from 'vue-i18n';
-import { ref } from 'vue';
+import { Ref, ref } from 'vue';
+import { RouteParamsRaw, useRouter } from 'vue-router';
 
 // Import library utilities, interfaces and components
+import { HandleObject } from 'src/utilities/HandleObject';
+import { HandleRoute } from 'src/utilities/HandleRoute';
+import { TDialog } from 'src/interfaces/BaseInterface';
 import PageTitle from 'src/components/PageTitle.vue';
 import PageDescription from 'src/components/PageDescription.vue';
 import PageLoading from 'src/components/PageLoading.vue';
+import BasicTable from 'src/components/BasicTable.vue';
 
 // Import Pinia's related utilities
+import { useConfigurationResourceStore } from 'src/stores/settings/configuration_resources';
+
+// Instantiate the pinia store
+const configurationResourceStore = useConfigurationResourceStore();
 
 // Defined the translation variable
 const { t } = useI18n({});
 
 const loadPage = ref(false);
+
+// Get all records
+configurationResourceStore.handleIndex('paginate');
+
+// Display the action name & dialog
+const actionName: Ref<TDialog | undefined> = ref(undefined);
+const displayDialog = ref(false);
+const disableActionDialogButton: Ref<{
+  action: TDialog | undefined;
+  disable: boolean;
+}> = ref({ action: undefined, disable: false });
+
+// Handle open dialog
+async function handleOpenDialog(action: TDialog, recordId?: number): Promise<void> {
+  const checkObject = new HandleObject();
+  loadPage.value = true;
+  switch (action) {
+    case 'create':
+      actionName.value = action;
+      loadPage.value = false;
+      handleNavigateToPage(action);
+      break;
+    case 'advanced-filters':
+      actionName.value = action;
+      loadPage.value = false;
+      displayDialog.value = true;
+      if (configurationResourceStore.getFilterModel.length === 0) {
+        disableActionDialogButton.value = {
+          action: action,
+          disable: true,
+        };
+      }
+      break;
+    case 'upload':
+      actionName.value = action;
+      loadPage.value = false;
+      displayDialog.value = true;
+      if (configurationResourceStore.getUploadModel.length === 0) {
+        disableActionDialogButton.value = {
+          action: action,
+          disable: true,
+        };
+      }
+      break;
+    case 'download':
+      actionName.value = action;
+      loadPage.value = false;
+      displayDialog.value = true;
+      if (configurationResourceStore.getDownloadModel.length === 0) {
+        disableActionDialogButton.value = {
+          action: action,
+          disable: true,
+        };
+      }
+      break;
+    case 'stats':
+      actionName.value = action;
+      loadPage.value = false;
+      displayDialog.value = true;
+      break;
+    case 'quick-show':
+      actionName.value = action;
+      configurationResourceStore.handleShow(recordId).then(() => {
+        loadPage.value = false;
+        displayDialog.value = true;
+      });
+      break;
+    case 'quick-edit':
+      actionName.value = action;
+      if (configurationResourceStore.getDataModel.length === 0) {
+        disableActionDialogButton.value = {
+          action: action,
+          disable: true,
+        };
+      } else {
+        configurationResourceStore.handleShow(recordId).then(() => {
+          loadPage.value = false;
+          displayDialog.value = true;
+        });
+      }
+      break;
+    case 'delete':
+      actionName.value = action;
+      configurationResourceStore.handleShow(recordId).then(() => {
+        loadPage.value = false;
+        displayDialog.value = true;
+      });
+      break;
+    case 'restore':
+      actionName.value = action;
+      configurationResourceStore.handleIndex('restore').then(() => {
+        if (
+          !checkObject.handleCheckIfObject(
+            configurationResourceStore.getAllDeletedRecords
+          ) &&
+          !checkObject.handleCheckIfArray(
+            configurationResourceStore.getAllDeletedRecords.results
+          )
+        ) {
+          disableActionDialogButton.value = {
+            action: action,
+            disable: true,
+          };
+        }
+        loadPage.value = false;
+        displayDialog.value = true;
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+// Handle action dialog
+async function handleActionDialog(action: TDialog): Promise<void> {
+  loadPage.value = true;
+  switch (action) {
+    case 'create':
+      configurationResourceStore.handleCreate().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'advanced-filters':
+      configurationResourceStore.handleAdvancedFilter('paginate').then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'upload':
+      configurationResourceStore.handleUpload().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'download':
+      configurationResourceStore.handleDownload().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'restore':
+      configurationResourceStore.handleRestore().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'quick-show':
+      displayDialog.value = false;
+      loadPage.value = false;
+      break;
+    case 'quick-edit':
+      configurationResourceStore.handleUpdate().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'delete':
+      configurationResourceStore.handleDelete().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    case 'stats':
+      configurationResourceStore.handleStats().then(() => {
+        displayDialog.value = false;
+        loadPage.value = false;
+      });
+      break;
+    default:
+      break;
+  }
+}
+
+// Go to Configure resource
+const router = useRouter();
+
+// Handle navigate to page
+const handleNavigateToPage = (action: TDialog) => {
+  const navigateToRoute = new HandleRoute();
+  let actionWords;
+  let actionName;
+  if (action.includes('-')) {
+    actionWords = action.split('-');
+    actionName = actionWords[1].charAt(0).toUpperCase() + actionWords[1].slice(1);
+    const selectedRecordId = configurationResourceStore.getSingleRecord.results[0].id;
+    navigateToRoute.handleNavigateToRoute(
+      router,
+      `AdminManagementContent${actionName}Page`,
+      ({ id: selectedRecordId } as unknown) as RouteParamsRaw
+    );
+  } else {
+    actionWords = action;
+    actionName = actionWords.charAt(0).toUpperCase() + actionWords.slice(1);
+    navigateToRoute.handleNavigateToRoute(
+      router,
+      `AdminManagementContent${actionName}Page`
+    );
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
