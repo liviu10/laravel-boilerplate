dd1b1cd892818d9f34763ba56894411e25d50965
Liviu Voica <liviu.voica@teilor.ro>
2024-05-16 14:16:05 +0300

    Bug fixing image responsive

--- resources/views/components/guest-slider-image.blade.php
+++ resources/views/components/guest-slider-image.blade.php
@@ -10,7 +10,7 @@
             </div>
         </div>
     </div>
-    <div class="col-lg-10 guest__slider-image-responsive">
+    <div class="col-10 guest__slider-image-responsive">
         <img src="{{ asset('images/14_poza_gradina_01_500x500px.png') }}" alt="">
         <img src="{{ asset('images/15_poza_gradina_02_500x500px.png') }}" alt="">
         <img src="{{ asset('images/16_poza_gradina_03_500x1050px.png') }}" alt="">
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('guest');
+    return view('pages.guest');
 })->name('guest.index');
 
 Auth::routes();
