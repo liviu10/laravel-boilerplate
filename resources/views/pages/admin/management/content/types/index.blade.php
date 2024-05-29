e78304a748a8e23b5b42444b538fcd1e8e104fbf
Liviu Voica <liviu.voica@teilor.ro>
2024-05-29 17:21:09 +0300

    improvements

--- resources/views/pages/admin/management/content/types/index.blade.php
+++ resources/views/pages/admin/management/content/types/index.blade.php
@@ -11,7 +11,21 @@
         </div>
 
         <div class="admin__body">
+            @if (Session::get('success'))
+            <div class="alert alert-success alert-dismissible fade show" role="alert">
+                <p class="my-0">
+                    {{ __('The record was successfully saved') }}
+                </p>
+                <button
+                    type="button"
+                    class="btn btn-close"
+                    data-bs-dismiss="alert"
+                    aria-label="Close"
+                />
+            </div>
+            @endif
 
+            {{ $data['results'] }}
         </div>
     </div>
 @endsection
