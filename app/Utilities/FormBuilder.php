613301c78979e6a93fff8c16e6b11be994935149
Liviu Voica <voica.liviu10@gmail.com>
2024-06-07 08:23:34 +0300

    Work in progress form builder

--- app/Utilities/FormBuilder.php
+++ app/Utilities/FormBuilder.php
@@ -2,6 +2,9 @@
 
 namespace App\Utilities;
 
+use App\Utilities\FormBuilderInputTypes;
+use App\Utilities\FormBuilderInterface;
+use App\Utilities\FormBuilderMethods;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Exception;
@@ -9,7 +12,7 @@ use Illuminate\Http\Response;
 use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Route;
 
-class FormBuilder
+class FormBuilder implements FormBuilderInterface
 {
     /**
      * Create a new class instance.
@@ -22,11 +25,121 @@ class FormBuilder
     }
 
     /**
-     * Handle the response.
+     * Handle the form builder.
      *
      * @return array
      */
-    public function handleFormBuilder(): array
+    public function handleFormBuilder(array $inputs): array
+    {
+        return [];
+    }
+
+    private function buildButtonInput(): array
+    {
+        return [];
+    }
+
+    private function buildCheckboxInput(): array
+    {
+        return [];
+    }
+
+    private function buildColorInput(): array
+    {
+        return [];
+    }
+
+    private function buildDateInput(): array
+    {
+        return [];
+    }
+
+    private function buildDateTimeLocalInput(): array
+    {
+        return [];
+    }
+
+    private function buildEmailInput(): array
+    {
+        return [];
+    }
+
+    private function buildFileInput(): array
+    {
+        return [];
+    }
+
+    private function buildHiddenInput(): array
+    {
+        return [];
+    }
+
+    private function buildImageInput(): array
+    {
+        return [];
+    }
+
+    private function buildMonthInput(): array
+    {
+        return [];
+    }
+
+    private function buildNumberInput(): array
+    {
+        return [];
+    }
+
+    private function buildPasswordInput(): array
+    {
+        return [];
+    }
+
+    private function buildRadioInput(): array
+    {
+        return [];
+    }
+
+    private function buildRangeInput(): array
+    {
+        return [];
+    }
+
+    private function buildResetInput(): array
+    {
+        return [];
+    }
+
+    private function buildSearchInput(): array
+    {
+        return [];
+    }
+
+    private function buildSubmitInput(): array
+    {
+        return [];
+    }
+
+    private function buildTelInput(): array
+    {
+        return [];
+    }
+
+    private function buildTextInput(): array
+    {
+        return [];
+    }
+
+    private function buildTimeInput(): array
+    {
+        return [];
+    }
+
+    private function buildUrlInput(): array
+    {
+        return [];
+    }
+
+    private function buildWeekInput(): array
     {
         return [];
     }
---
+++ app/Utilities/FormBuilderInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Utilities;
+
+
+
+interface FormBuilderInterface
+{
+    public function handleFormBuilder(array $inputs): array;
+}
